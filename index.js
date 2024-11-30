9cfa71329e23d485c65097bc323e720dd339951b
sahil <marinermantosh@gmail.com>
2024-11-30 13:10:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.getElementById('check').addEventListener('change', function() {
+//     const dropdownMenu = document.querySelector('.nav_drop');
+//     if (this.checked) {
+//         dropdownMenu.style.display = 'block';
+//     } else {
+//         dropdownMenu.style.display = 'none';
+//     }
+// });
